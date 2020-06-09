e3047b8beac0edf747f923bd891cf0a43832c226
xb <906188578@qq.com>
2020-06-09 14:42:43 +0800

    feat: externals插件

---
+++ packages/auto-externals-webpack-plugin/README.md
@@ -0,0 +1,11 @@
+# `auto-externals-webpack-plugin`
+
+> TODO: description
+
+## Usage
+
+```
+const autoExternalsWebpackPlugin = require('auto-externals-webpack-plugin');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/auto-externals-webpack-plugin/lib/index.js
@@ -0,0 +1,55 @@
+const ExternalPlugin = require('webpack/lib/ExternalModule');
+const PLUGIN_ID = 'AutoExternalWebpackPlugin';
+
+class AutoExternalWebpackPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+  apply(compiler) {
+    compiler.hooks.normalModuleFactory.tap(PLUGIN_ID, (normalModuleFactory) => {
+      normalModuleFactory.hooks.factory.tap(
+        PLUGIN_ID,
+        (factory) => (data, callback) => {
+          const dependency = data.dependencies[0];
+          const moduleName = dependency.request;
+          const config = this.options[moduleName];
+          if (config) {
+            // 外部引用模块，不需要打包
+            callback(
+              null,
+              new ExternalPlugin(config.variable, 'windows', dependency.request)
+            );
+          } else {
+            factory(data, callback);
+          }
+        }
+      );
+    });
+
+    compiler.hooks.compilation.tap(PLUGIN_ID, (compilation) => {
+      compilation.hooks.htmlWebpackPluginAlterAssetTags.tap(
+        PLUGIN_ID,
+        (assetsTags) => {
+          const { options } = this;
+          const objArr = Object.keys(options).map((key) => {
+            return {
+              tagName: 'script',
+              selfClose: false,
+              attributes: {
+                type: 'text/javascript',
+                src: options[key].src,
+              },
+            };
+          });
+
+          return {
+            ...assetsTags,
+            body: [...objArr, ...assetsTags.body],
+          };
+        }
+      );
+    });
+  }
+}
+
+module.exports = AutoExternalWebpackPlugin;
